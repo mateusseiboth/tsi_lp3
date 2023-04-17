e8ba49135fec33a0661f869d5f097e8a05e4a316
mateusseiboth <mateusseiboth@gmail.com>
2023-04-17 08:45:53 -0400

    Upload de arquivos faltantes

---
+++ src/main/java/br/edu/ifms/controller/AdminController.java
@@ -0,0 +1,92 @@
+package br.edu.ifms.controller;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.edu.ifms.controller.util.DataModelling;
+import br.edu.ifms.dao.UserDAO;
+import br.edu.ifms.dao.util.Connect;
+import br.edu.ifms.model.User;
+
+/**
+ * Servlet implementation class IndexControle
+ */
+@WebServlet("/auth/admin")
+public class AdminController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO userDAO;
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AdminController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void init(){
+		// TODO Auto-generated method stub
+		userDAO = new UserDAO();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		procRequest(request, response);
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		procRequest(request, response);
+	}
+
+	private void procRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String acao = request.getParameter("acao");
+		try {
+			switch (acao) {
+			case "listar":
+				listUser(request, response);
+				break;
+	
+			}
+			
+		} catch (Exception ex) {
+			throw new ServletException(ex);
+		}
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	
+	private void listUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, ServletException, IOException{
+		List<User> users = userDAO.listUsers();
+		request.setAttribute("listUser", users);
+		RequestDispatcher dispatcher = request.getRequestDispatcher(request.getServletPath() + "/admin-list-user.jsp");
+		
+		dispatcher.forward(request, response);
+		
+	}
+	
+
+}
---
+++ src/main/webapp/auth/admin/admin-list-user.jsp
@@ -0,0 +1,59 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>BiblioCraft - Cadastrar usuário</title>
+</head>
+<body class="bg-dark">
+
+	<jsp:include page="../../component/navbar.jsp" />
+
+	<div class="container text-white">
+		<div class="row">
+			<div class="col">
+				<h2>Listagem de usuários</h2>
+			</div>
+		</div>
+		<div class="row">
+		<c:forEach items="${listUser}" var="user">
+		<div class='col-sm-3 col-md-3 col-lg-3' style='margin-top: 2rem;'>
+			<div id='cartao' class='card bg-dark'>
+
+				<div class='card-header' style='text-align: center;'>
+					<h5 class='card-title'>${user.name}</h5>
+				</div>
+				<div class='card-body' style='text-align: center;'>
+
+					<div class='mb-3'>
+						<label for='placa' class='form-label'>CPF: </label>
+						<div class='input-group col-md-6 mb-3'>
+							<p>${user.cpf}</p>
+						</div>
+					</div>
+					<div class='mb-3'>
+						<label for='preco' class='form-label'>Email:</label>
+						<div class='input-group col-md-6 mb-3'>
+							<p>${user.email}</p>
+						</div>
+						<div class='mb-6'>
+							<button type='button' class='btn btn-primary btn-editar'
+								data-bs-toggle='modal' data-bs-target='#myModal'
+								data-id='{$cliente[' id']}' data-nome='{$cliente['
+								nome']}' data-cpf='{$cliente['
+								cpf']}' data-telefone='{$cliente['telefone']}''>Editar
+							</button>
+							<a href='$pathExcluir/{$cliente[' id']}' class='btn btn-danger'>Excluir</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</c:forEach>
+		</div>
+	</div>
+
+</body>
+</html>
