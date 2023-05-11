64472f545803582e809e2b74c007db4ab7a5bac8
mateusseiboth <mateusseiboth@gmail.com>
2023-05-10 21:46:50 -0400

    Adição de classe de funções

---
+++ src/main/java/br/edu/ifms/model/Roles.java
@@ -0,0 +1,20 @@
+package br.edu.ifms.model;
+
+import java.util.List;
+
+public class Roles {
+	
+	private Long id;
+	
+	private String typeRole;
+	
+	private List<User> users;
+	
+	
+	public Roles() {
+		
+	}
+	
+	
+
+}
